5a935a97dfd646d2acd1cac7f8aba41f5d105b4b
sinjinchang <1967220808@qq.com>
2023-10-25 15:25:49 -0400

    day 3

--- src/block/builder.rs
+++ src/block/builder.rs
@@ -1,27 +1,43 @@
 use bytes::BufMut;
-use super::{Block,SIZEOF_16};
+
+use super::{Block, SIZEOF_U16};
 
 /// Builds a block.
 pub struct BlockBuilder {
-    offset: Vec<u16>,
-    data: Vec<u16>,
+    /// Offsets of each key-value entries.
+    offsets: Vec<u16>,
+    /// All key-value pairs in the block.
+    data: Vec<u8>,
+    /// The expected block size.
     block_size: usize,
 }
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        Self { offset: Vec::new(), data: Vec::new(), block_size}
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+        }
+    }
+
+    fn estimated_size(&self) -> usize {
+        self.offsets.len() * SIZEOF_U16 + self.data.len() + SIZEOF_U16
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
         assert!(!key.is_empty(), "key must not be empty");
-        if self.estimated_size() + key.len() + value.len() +SIZEOF_16 * 3 > self.block_size && !self.is_empty(){
+        // The overhead here is `key_len` + `val_len` + `offset`, each is of type `u16`
+        if self.estimated_size() + key.len() + value.len() + SIZEOF_U16 * 3 > self.block_size
+            && !self.is_empty()
+        {
             return false;
         }
-        self.offset.push(self.data.len() as u16);
+        // The offsets should be updated at first, to maintain the correct offset
+        self.offsets.push(self.data.len() as u16);
         self.data.put_u16(key.len() as u16);
         self.data.put(key);
         self.data.put_u16(value.len() as u16);
@@ -31,7 +47,7 @@ impl BlockBuilder {
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        self.offset.is_empty()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
@@ -43,5 +59,5 @@ impl BlockBuilder {
             data: self.data,
             offsets: self.offsets,
         }
-     }
+    }
 }
